a3c1ddcc2f8bcd03183b3f387548f7529ce34111
utkarshey <utkarsh.kant@gds.ey.com>
2023-11-08 18:49:46 +0530

    requirements.added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "poleDetection",
+    version = "0.0.0",
+    author = "Utkarsh Kant",
+    author_email = "utkarsh.kant@gds.ey.com",
+    packages = find_packages(),
+    install_requires = []
+)
